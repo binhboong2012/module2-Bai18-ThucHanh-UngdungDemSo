0c4aa1864884db052634b2529b56798071fe0c3c
binhboong2012 <binhcauduongbo@gmail.com>
2022-01-27 16:26:10 +0700

    Ung dung dem so

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,19 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Count count = new Count() ;
+        try {
+            while (count.getMyThread().isAlive()) {
+                System.out.println("Main thread will be alive till the child thread is live");
+                Thread.sleep(1000);
+            }
+
+        }
+        catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("Main thread run is over");
+    }
+}
